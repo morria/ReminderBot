6db8e4a754da2dc2fd153bc032e1ad8acbc54fdf
Andrew Morrison <amorrison@etsy.com>
2013-02-03 17:58:19 -0500

    adding an as a time amount

--- src/main/scala/com/etsy/TimeOffset.scala
+++ src/main/scala/com/etsy/TimeOffset.scala
@@ -39,10 +39,11 @@ object TimeOffset {
    * Get a TimeOffset from a string such as "5 seconds"
    */
   def apply(message:String) : Option[TimeOffset] =
-    """(a|one|two|a couple|a few|[0-9]+)\s+((sec(ond)?|min(ute)?|hour|day|week|month)s?)""".r.findFirstMatchIn(message.toLowerCase) match {
+    """(a|an|one|two|a couple|a few|[0-9]+)\s+((sec(ond)?|min(ute)?|hour|day|week|month)s?)""".r.findFirstMatchIn(message.toLowerCase) match {
       case Some(m:Match) =>
         val amount:Int = m.group(1) match {
           case "a" => 1
+          case "an" => 1
           case "one" => 1
           case "two" => 2
           case "a couple" => 2
